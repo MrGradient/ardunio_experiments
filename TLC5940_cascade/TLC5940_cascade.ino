659172af590387c59e4ef25cccf26e1c7dfb8d99
Sebastian Chlan <sebastian.chlan@gmail.com>
2015-10-12 21:03:33 +0100

    PWM frequency

--- TLC5940_cascade/TLC5940_cascade.ino
+++ TLC5940_cascade/TLC5940_cascade.ino
@@ -1,5 +1,3 @@
-#include <SPI.h>
-
 #define N_TLC  2
 
 #define SIN   11
@@ -10,17 +8,21 @@
 #define DCPRG  3
 #define GSCLK  9
 
-const uint8_t DC = B00111111;
-
 volatile uint8_t gsUpdateFlag;
+
+const int DC = B00111111;
 
 int dcData[12 * N_TLC] = {0};
 int gsData[24 * N_TLC] = {0};
 
 void setup() {
+  noInterrupts();
   Serial.begin(9600);
   while (!Serial) {}
   Serial.println("Setup begin");
+
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
   
   pinMode(SIN, OUTPUT);
   pinMode(SCLK, OUTPUT);
@@ -39,25 +41,27 @@ void setup() {
   digitalWrite(BLANK, HIGH);
   Serial.println("Pin level set");
 
-  SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV2, MSBFIRST, SPI_MODE0));
-  SPI.begin();
+ 
+  SPCR = (1 << SPE) | (1 << MSTR);
+  SPSR = (1 << SPI2X);
   Serial.println("Setup done");
-
-  clockInDotCorrection();
-
-  noInterrupts();
+ 
   TCCR0A = (1 << WGM01);
   TCCR0B = ((1 << CS02) | (1 << CS00));
-  OCR0A = 254;
+  OCR0A = 31;
   TIMSK0 |= (1 << OCIE0A);
 
-  TCCR1B |= (1 << CS11); 
-  TCCR1A |= ((1<<WGM11) | (1<<WGM10));
-  TCCR1B |= ((1<<WGM13) | (1<<WGM12));
-  TCCR1A |= (1 << COM1A0);
-  TCNT1 = 0;
-
+  // Timer 1 - 4MHz, Duty cycle 50%
+  TCCR1A=0;
+  TCCR1B=0;
+  TCCR1A |= _BV(WGM11);
+  TCCR1B |= _BV(WGM13)|_BV(CS10);
+  OCR1A = 1;
+  ICR1 = 2;
+  TCCR1A |= _BV(COM1A1);
+  
   interrupts();
+  clockInDotCorrection();
   Serial.println("Go");
 }
 
@@ -75,10 +79,12 @@ void clockInDotCorrection() {
 
   digitalWrite(DCPRG, HIGH);
   digitalWrite(VPRG, HIGH);
-
+  
   setAllDc(DC);
+
   for(int i = 0; i < 12 * N_TLC; i++){
-    SPI.transfer(dcData[i]);
+    SPDR = dcData[i];
+    while (!(SPSR & (1 << SPIF))){};
   }
   
   Serial.println("Dot correction sent");
@@ -93,28 +99,29 @@ ISR(TIMER0_COMPA_vect) {
 
   if(digitalRead(VPRG) == 1) {
     digitalWrite(VPRG, LOW);
+
     if(xlatNeedsPulse == 1){
       pulse(XLAT);
       xlatNeedsPulse = 0;
     }
-    
-    } else if (xlatNeedsPulse == 1) {
-      pulse(XLAT);
-      xlatNeedsPulse = 0;
-    }
-    
-    digitalWrite(BLANK, LOW);
 
-    if(gsUpdateFlag == 1){
-      for(int i = 0; i < N_TLC * 24; i++){
-        SPI.transfer(gsData[i]);
-      }
-      xlatNeedsPulse = 1;
-      gsUpdateFlag = 0;
-    }
-    
-    
+  } else if (xlatNeedsPulse == 1) {
+    pulse(XLAT);
+    xlatNeedsPulse = 0;
   }
+    
+  digitalWrite(BLANK, LOW);
+
+  if(gsUpdateFlag == 1){
+    for(int i = 0; i < N_TLC * 24; i++){
+      SPDR = gsData[i];
+    // Wait for transmission complete
+      while (!(SPSR & (1 << SPIF)));
+    }
+    xlatNeedsPulse = 1;
+    gsUpdateFlag = 0;
+  }
+} 
 
 void setAllDc(uint8_t value) {
   uint8_t tmp1 = (uint8_t)(value << 2);
@@ -131,8 +138,9 @@ void setAllDc(uint8_t value) {
   } while (i < 12 * N_TLC);
 } 
 
+
 void setGs(int channel, uint16_t value) {
-  channel = (16 * N_TLC) - 1 - channel;
+  channel = (16 * 2) - 1 - channel;
   uint16_t i = (uint16_t)channel * 3 / 2;
   switch (channel % 2) {
     case 0:
@@ -150,53 +158,48 @@ void setGs(int channel, uint16_t value) {
 
 int16_t count = 0;
 uint16_t level [16 * N_TLC] = {
- 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 11, 11, 11, 12, 11, 11, 11, 11, 10, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 11, 11, 11, 12, 11, 11, 11, 11, 10, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1
 };
 
-long brightness = 0L;
+int brightness = 0;
 int8_t factor = 10;
 
 void loop (){
 
-  if(count < 20000){
+  if(count < 9000){
     count++;
     return;
   } else {
-      count = 0;
+    count = 1;
   }
-    
+
   int tmp = level[0];
   for(int i = 0; i < (16 * N_TLC - 1); i++){
     level[i] = level[i+1];
   }
   level[16 * N_TLC - 1] = tmp;
 
-//  if(brightness >= 4095){
-//    factor = abs(factor) * -1;
-//  } else if(brightness <= 0){
-//    factor = abs(factor);
-//  }
+  if(brightness >= 4095){
+    factor = abs(factor) * -1;
+  } else if(brightness <= 0){
+    factor = abs(factor);
+  }
 
+  brightness += factor;
 
-//  if(brightness < 0) {
-//    brightness = 0;
-//    Serial.println("Min");
-//  }
-//  if(brightness > 4095) {
-//    brightness = 4095;
-//    Serial.println("Max");
-//  }
+  if(brightness < 0) {
+    brightness = 0;
+    Serial.println("Min");
+  }
+  if(brightness > 4095) {
+    brightness = 4095;
+    Serial.println("Max");
+  }
 
   while(gsUpdateFlag);
   for(int i = 0; i < 16 * N_TLC; i++){
-    setGs(i, (1L << level[i])-1);
-    //setGs(i, (uint16_t)brightness);
+    setGs(i, (1L << level[i]) - 1);
+    //setGs(i, brightness);
   }
   setGsUpdateFlag();
 }
-
-
-
-
-
-
